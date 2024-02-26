89a4f454b1de243cfde2f0a51ccfa19d9b9ad23f
Inish2609 <inishraj.b.s003@gmail.com>
2024-02-26 14:39:55 +0530

    Added Secure Password Method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_070259) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_090514) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_070259) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
